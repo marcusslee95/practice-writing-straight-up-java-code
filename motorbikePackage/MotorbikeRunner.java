b09d27d0439c06a5221a77c228a1cb708ae0825a
Marcus Lee <marcusslee95@berkeley.edu>
2021-04-02 00:11:58 -0700

    before made method for some code

--- motorbikePackage/MotorbikeRunner.java
+++ motorbikePackage/MotorbikeRunner.java
@@ -7,8 +7,16 @@ class MotorbikeRunner{
 		System.out.println(harleyDavidson.getSpeed());
 
 		// harleyDavidson.speed = 9000;
-		harleyDavidson.setSpeed(-1000);
+		harleyDavidson.setSpeed(1000);
 		// System.out.println(harleyDavidson.speed);
+		System.out.println(harleyDavidson.getSpeed());
+
+		int harleyDavidsonSpeed = harleyDavidson.getSpeed();
+		harleyDavidson.setSpeed(harleyDavidsonSpeed + 666);
+		System.out.println(harleyDavidson.getSpeed());
+
+		int harleyDavidsonNewSpeed = harleyDavidson.getSpeed();
+		harleyDavidson.setSpeed(harleyDavidsonNewSpeed - 666);
 		System.out.println(harleyDavidson.getSpeed());
 	}
 }
