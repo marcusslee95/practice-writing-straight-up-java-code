8dfb25f335882b8cfa761ad4650c0d1c2eb67744
Marcus Lee <marcusslee95@berkeley.edu>
2021-04-01 23:17:27 -0700

    added a method and called it

--- Motorbike.java
+++ Motorbike.java
@@ -1,7 +1,13 @@
 class Motorbike{
 
+	void start(){
+		System.out.println("Motorbike Started");
+	}
+
 	public static void main(String[] args){
 		Motorbike harleyDavidson = new Motorbike();
 		Motorbike ducati = new Motorbike();
+		harleyDavidson.start();
+		ducati.start();
 	}
 }
