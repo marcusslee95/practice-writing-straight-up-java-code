a9022967eba20b964168d96c5d1bd633a631081f
Marcus Lee <marcusslee95@berkeley.edu>
2021-04-01 23:21:56 -0700

    added a property and set it to a value

--- Motorbike.java
+++ Motorbike.java
@@ -1,13 +1,19 @@
 class Motorbike{
 
+	int speed;
+
 	void start(){
 		System.out.println("Motorbike Started");
 	}
 
 	public static void main(String[] args){
 		Motorbike harleyDavidson = new Motorbike();
-		Motorbike ducati = new Motorbike();
+		// Motorbike ducati = new Motorbike();
 		harleyDavidson.start();
-		ducati.start();
+		// ducati.start();
+		System.out.println(harleyDavidson.speed);
+
+		harleyDavidson.speed = 9000;
+		System.out.println(harleyDavidson.speed);
 	}
 }
