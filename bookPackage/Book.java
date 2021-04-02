56338f40dece168f184f5a1dcaae4e85b157e0f0
Marcus Lee <marcusslee95@berkeley.edu>
2021-04-02 08:44:39 -0700

    added exception handling

--- bookPackage/Book.java
+++ bookPackage/Book.java
@@ -1,15 +1,32 @@
 public class Book {
 	private String name;
+	private int numOfPages;
 
-	Book (String name){
+	Book (String name, int numOfPages){
 		this.name = name;
+		this.numOfPages = numOfPages;
 	}
 
 	public String toString(){
-		return this.name;
+		return this.name + "; " + this.numOfPages;
 	}
 
 	public void setName(String whatToChangeNameTo){
 		this.name = whatToChangeNameTo;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public void setNumOfPages(int whatToChangeNumOfPagesTo){
+		if (whatToChangeNumOfPagesTo < 0){
+			throw new RuntimeException("Can't have a book having - pages");
+		}
+		this.numOfPages = whatToChangeNumOfPagesTo;
+	}
+
+	public int getNumOfPages(){
+		return this.numOfPages;
 	}
 }
--- bookPackage/BookRunner.java
+++ bookPackage/BookRunner.java
@@ -1,13 +1,26 @@
 public class BookRunner {
 
 	public static void main (String[] args){
-		Book artOfProgramming = new Book("Art Of Programming");
-		Book cleanCode = new Book("Clean Code");
+		Book artOfProgramming = new Book("Art Of Programming", 500);
+		Book cleanCode = new Book("Clean Code", 300);
 		System.out.println(artOfProgramming.toString());
 		System.out.println(cleanCode.toString());
 
 		// artOfProgramming.name = "whackflocka";
 		artOfProgramming.setName("whackflocka");
+		System.out.println(artOfProgramming.getName());
+
+		artOfProgramming.setNumOfPages(9000);
+		System.out.println(artOfProgramming.getNumOfPages());
 		System.out.println(artOfProgramming.toString());
+
+		// artOfProgramming.setNumOfPages(-1);
+		// System.out.println(artOfProgramming.getNumOfPages());
+		try{
+			artOfProgramming.setNumOfPages(-1);
+		} catch (RuntimeException e){
+			e.printStackTrace();
+		}
+		System.out.println(artOfProgramming.getNumOfPages());
 	}
 }
